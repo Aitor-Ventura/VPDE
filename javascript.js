cd5b82443ce2f8eae13221b7c92bd6d9464cd362
Aitor Ventura <aitor.ventura00@gmail.com>
2023-02-18 21:25:34 +0000

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 ﻿const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-const info = document.querySelector("p");
+const info = document.querySelector("#p");
 
 const GLOBALS = {
     charX: 0,
